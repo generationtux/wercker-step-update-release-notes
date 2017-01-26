5ab70c8b6f736e1da1a7b21444069a01c502c48e
thomas Bell <thomasbell@Thomas-Macbook.local>
2017-01-26 12:51:01 -0500

    added properties updated run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 STORY_NUMBER=`git log -1 HEAD --pretty=format:%s | grep -Po '(?<=ch)(\d+)'`
 
 if [ -z $STORY_NUMBER ] ; then
-    success "No release specified, tha ts not a problem, just letting you know."
-fi
-if [ $STORY_NUMBER ] ; then
+    success "No release specified, thats not a problem, just letting you know."
+
+else
     curl -v -H "x-api-key: $WERCKER_NOTIFY_RELEASE_NOTES_API_KEY" \
         -H 'Content-Type: application/json' \
         "$WERCKER_NOTIFY_RELEASE_NOTES_API_URL/release/$STORY_NUMBER" &&
